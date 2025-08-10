14f9c42b99fb623dd577e477f4fb12aaf44095f2
iokira <iam.iokiller@gmail.com>
2025-08-11 00:08:54 +0900

    indent

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,12 +2,12 @@ import styles from "./index.module.scss";
 
 export const Navigation = () => {
     return (
-<nav className={styles["nav-links"]}>
-    <a href="/">iokira.net</a>
-    <div className={styles["links"]}>
-        <a href="/about">about</a>
-        <a href="/blog">blog</a>
-    </div>
-</nav>
+        <nav className={styles["nav-links"]}>
+            <a href="/">iokira.net</a>
+            <div className={styles["links"]}>
+                <a href="/about">about</a>
+                <a href="/blog">blog</a>
+            </div>
+        </nav>
     )
 }
